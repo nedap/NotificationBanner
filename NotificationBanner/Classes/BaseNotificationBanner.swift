0c2cf4dc8d121a2e6c011f1a515bafad10c3d5ef
Yulia Mykhailova <yulia.mykhailova@nedap.com>
2020-04-24 16:45:24 +0200

    Moved magic numbers to variables, fixed calculation of the height for growing banners.

--- NotificationBanner/Classes/BaseNotificationBanner.swift
+++ NotificationBanner/Classes/BaseNotificationBanner.swift
@@ -145,6 +145,11 @@ open class BaseNotificationBanner: UIView {
     /// The default offset for spacerView top or bottom
     internal var spacerViewDefaultOffset: CGFloat = 10.0
 
+    /// The default height of the banner
+    internal var bannerDefaultHeight: CGFloat {
+        return 64.0
+    }
+
     /// The maximum number of banners simultaneously visible on screen
     internal var maximumVisibleBanners: Int = 1
 
@@ -290,7 +295,7 @@ open class BaseNotificationBanner: UIView {
     }
 
     internal func spacerViewHeight() -> CGFloat {
-        return basicYOffset + 10
+        return spacerViewDefaultOffset + basicYOffset
     }
 
     private func finishBannerYOffset() -> CGFloat {
@@ -677,7 +682,7 @@ open class BaseNotificationBanner: UIView {
      */
 
     internal func basicHeight() -> CGFloat {
-        return 64.0 + basicYOffset
+        return bannerDefaultHeight + basicYOffset
     }
 
     /**
